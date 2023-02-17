e675777f8042c1e8eed9500396de76c5427a0b08
captainko <thongnguyen.uit@gmail.com>
2023-02-17 22:26:26 +0700

    refactor(barbecue): move event to the top

--- lua/core/plugins/barbecue.lua
+++ lua/core/plugins/barbecue.lua
@@ -1,10 +1,11 @@
+---@type LazyPlugin
 local M = {
 	"utilyre/barbecue.nvim",
+	event = { "BufReadPost" },
 	dependencies = {
 		"SmiteshP/nvim-navic",
 		"nvim-tree/nvim-web-devicons", -- optional dependency
 	},
-	event = { "BufReadPost" },
 }
 
 function M.config()
