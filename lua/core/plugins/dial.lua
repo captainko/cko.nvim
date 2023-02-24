034d9f1ba6a36bc2fa5e7d8130307ee5024c5160
captainko <thongnguyen.uit@gmail.com>
2023-02-24 16:08:19 +0700

    refactor(dial): use `on_filetype` method

--- lua/core/plugins/dial.lua
+++ lua/core/plugins/dial.lua
@@ -11,105 +11,110 @@ function M.config()
 	local nnoremap = mapper.nnoremap
 	local vnoremap = mapper.vnoremap
 
-	--- mapping dial functions to a specific group
-	---@param group string?
-	---@param bufnr number?
-	local function create_dial_mappings(group, bufnr)
-		bufnr = bufnr or vim.api.nvim_get_current_buf()
-		nnoremap({ "<C-A>", dial.inc_normal(group), buffer = bufnr })
-		nnoremap({ "<C-X>", dial.dec_normal(group), buffer = bufnr })
-		vnoremap({ "<C-A>", dial.inc_visual(group), buffer = bufnr })
-		vnoremap({ "<C-X>", dial.dec_visual(group), buffer = bufnr })
-		vnoremap({ "g<C-A>", dial.inc_gvisual(group), buffer = bufnr })
-		vnoremap({ "g<C-X>", dial.dec_gvisual(group), buffer = bufnr })
-		-- vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-a>", require("dial.map").inc_normal(group), { noremap = false })
-	end
+	nnoremap({ "<C-A>", dial.inc_normal() })
+	nnoremap({ "<C-X>", dial.dec_normal() })
+	vnoremap({ "<C-A>", dial.inc_visual() })
+	vnoremap({ "<C-X>", dial.dec_visual() })
+	vnoremap({ "g<C-A>", dial.inc_gvisual() })
+	vnoremap({ "g<C-X>", dial.dec_gvisual() })
 
-	create_dial_mappings(nil, nil)
+	local default_augends = {
+		augend.integer.alias.decimal,
+		augend.integer.alias.hex,
+		augend.date.alias["%Y/%m/%d"],
+		augend.constant.alias.bool,
+		augend.constant.new({ elements = { "&&", "||" }, word = false, cyclic = true }),
+		augend.constant.new({ elements = { "and", "or" }, word = true, cyclic = true, preserve_case = true }),
+		augend.constant.new({ elements = { "yes", "no" }, word = true, cyclic = true, preserve_case = true }),
+		-- switch quotes
+		-- augend.paren.new({
+		-- 	patterns = { { "'", "'" }, { '"', '"' } },
+		-- 	nested = false,
+		-- 	escape_char = [[\]],
+		-- 	cyclic = true,
+		-- }),
+	}
 
-	local function get_default_augends()
-		return {
-			augend.integer.alias.decimal,
-			augend.integer.alias.hex,
-			augend.date.alias["%Y/%m/%d"],
-			augend.constant.alias.bool,
-			augend.constant.new({ elements = { "&&", "||" }, word = false, cyclic = true }),
-			augend.constant.new({ elements = { "and", "or" }, word = true, cyclic = true }),
-			augend.constant.new({ elements = { "yes", "no" }, word = true, cyclic = true }),
-		}
-	end
+	require("dial.config").augends:register_group({ default = default_augends })
+
+	---@type table<string, Augend[]>
+	local filetype_map = {}
 
 	---@param augends Augend[]
 	---@return Augend[]
 	local function with_default_augends(augends)
-		return vim.list_extend(get_default_augends(), augends)
+		return vim.list_extend(augends, default_augends)
 	end
 
-	local FRONTEND_KEY = "frontend_group"
-	local frontend_group = with_default_augends({
-		augend.constant.new({
-			elements = { "const", "let" },
-			word = true,
-			cyclic = true,
-		}),
-	})
+	---comment
+	---@param filetype string|string[]
+	---@param augends Augend[]
+	local function add_to_filetype_map(filetype, augends)
+		local filetypes = (type(filetype) == "string" and { filetype }) or filetype --[[@as string[] ]]
+		for _, ft in ipairs(filetypes) do
+			local augend_list = filetype_map[ft]
 
-	local BACKGROUND_KEY = "backend_group"
-	local backend_augends = with_default_augends({
-		augend.constant.new({
-			elements = { "public", "private", "protected" },
-			word = true,
-			cyclic = true,
-		}),
-	})
+			if augend_list == nil then
+				filetype_map[ft] = {}
+				augend_list = filetype_map[ft]
+			end
 
-	local GIT_COMMIT_KEY = "git_group"
-	local git_commit_augends = with_default_augends({
-		augend.constant.new({
-			elements = { "feat", "refactor", "fix", "enhance" },
-			word = true,
-			cyclic = true,
-		}),
-	})
+			vim.list_extend(augend_list, augends)
+		end
+	end
 
-	local DEPENDENCY_KEY = "dependency_group"
-	---@type Augend[]
-	local dependency_augends = {
+	add_to_filetype_map(
+		{ "javascript", "typescript", "vue", "html" },
+		with_default_augends({
+			augend.constant.new({
+				elements = { "const", "let" },
+				word = true,
+				cyclic = true,
+			}),
+			augend.constant.new({
+				elements = { "boolean", "string", "number", "object" },
+				word = true,
+				cyclic = true,
+			}),
+
+			augend.constant.new({
+				elements = { "Boolean", "String", "Number", "Object", "Array" },
+				word = true,
+				cyclic = true,
+			}),
+		})
+	)
+
+	add_to_filetype_map(
+		{ "java", "typescript", "csharp" },
+		with_default_augends({
+			augend.constant.new({
+				elements = { "public", "private", "protected" },
+				word = true,
+				cyclic = true,
+			}),
+		})
+	)
+
+	add_to_filetype_map({ "yaml", "toml", "json" }, {
 		augend.integer.alias.decimal,
 		augend.integer.alias.hex,
 		augend.constant.alias.bool,
 		augend.semver.alias.semver,
-	}
-
-	require("dial.config").augends:register_group({
-		-- default augends used when no group name is specified
-		default = get_default_augends(),
-		[FRONTEND_KEY] = frontend_group,
-		[BACKGROUND_KEY] = backend_augends,
-		[DEPENDENCY_KEY] = dependency_augends,
-		[GIT_COMMIT_KEY] = git_commit_augends,
 	})
 
-	vim.api.nvim_create_autocmd("FileType", {
-		pattern = { "vue", "html", "typescript", "javascript" },
-		callback = function(event)
-			create_dial_mappings(FRONTEND_KEY, event.buf)
-		end,
-	})
+	add_to_filetype_map(
+		"gitcommit",
+		with_default_augends({
+			augend.constant.new({
+				elements = { "feat", "refactor", "fix", "enhance" },
+				word = true,
+				cyclic = true,
+			}),
+		})
+	)
 
-	vim.api.nvim_create_autocmd("FileType", {
-		pattern = { "yaml", "toml", "json" },
-		callback = function(event)
-			create_dial_mappings(DEPENDENCY_KEY, event.buf)
-		end,
-	})
-
-	vim.api.nvim_create_autocmd("FileType", {
-		pattern = "gitcommit",
-		callback = function(event)
-			create_dial_mappings(GIT_COMMIT_KEY, event.buf)
-		end,
-	})
+	require("dial.config").augends:on_filetype(filetype_map)
 end
 
 return M
