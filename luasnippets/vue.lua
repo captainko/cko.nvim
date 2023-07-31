d9bdfd224fcd3ebf11f79fededd5be7e8bdd3d80
captainko <thongnguyen.uit@gmail.com>
2023-07-31 16:28:49 +0700

    feat(luasnippets)(vue): add `tryfinally`

--- luasnippets/vue.lua
+++ luasnippets/vue.lua
@@ -1,6 +1,8 @@
 local ls = require("luasnip")
 local s = ls.snippet
 local t = ls.text_node
+local i = ls.insert_node
+local fmt = require("luasnip.extras.fmt").fmt
 
 local M = {
 	s({ trig = "db" }, { t("debugger;") }),
@@ -11,6 +13,21 @@ local M = {
 			"}",
 		}),
 	}),
+	s(
+		{ trig = "tryfinally", name = "Try Finally" },
+		fmt(
+			[[try {{
+	{}
+}} finally {{
+	{}
+}}
+]],
+			{
+				i(1),
+				i(2),
+			}
+		)
+	),
 }
 
 return M
