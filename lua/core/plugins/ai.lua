4c2296f00884b1effb43554593495c148f3b5a82
captainko <thongnguyen.uit@gmail.com>
2023-12-01 08:37:24 +0700

    feat(ai): add gen.nvim

--- lua/core/plugins/ai.lua
+++ lua/core/plugins/ai.lua
@@ -1,27 +1,41 @@
 ---@type LazyPluginSpec[]
 local M = {
+	-- {
+	-- 	"jackMort/ChatGPT.nvim",
+	-- 	enabled = false,
+	-- 	cmd = { "ChatGPT", "ChatGPTActAs" },
+	-- 	dependencies = {
+	-- 		"MunifTanjim/nui.nvim",
+	-- 		"nvim-lua/plenary.nvim",
+	-- 		"nvim-telescope/telescope.nvim",
+	-- 	},
+	-- 	config = function()
+	-- 		require("chatgpt").setup({
+	-- 			-- optional configuration
+	-- 		})
+	-- 	end,
+	-- },
+	-- {
+	-- 	"huggingface/hfcc.nvim",
+	-- 	enabled = false,
+	-- 	cmd = { "HFccToggleAutoSuggest" },
+	-- 	config = function()
+	-- 		require("hfcc").setup({
+	-- 			model = "meta-llama/Llama-2-7b",
+	-- 		})
+	-- 	end,
+	-- },
 	{
-		"jackMort/ChatGPT.nvim",
-		enabled = false,
-		cmd = { "ChatGPT", "ChatGPTActAs" },
-		dependencies = {
-			"MunifTanjim/nui.nvim",
-			"nvim-lua/plenary.nvim",
-			"nvim-telescope/telescope.nvim",
-		},
+		"David-Kunz/gen.nvim",
+		cmd = { "Gen" },
+		keys = { { mode = { "v", "n" }, "<leader>]]", "<Cmd>Gen<CR>" } },
 		config = function()
-			require("chatgpt").setup({
-				-- optional configuration
-			})
-		end,
-	},
-	{
-		"huggingface/hfcc.nvim",
-		enabled = false,
-		cmd = { "HFccToggleAutoSuggest" },
-		config = function()
-			require("hfcc").setup({
-				model = "meta-llama/Llama-2-7b",
+			-- local gen = require("gen")
+			-- gen.container = "ollama" -- default nil
+			-- gen.model = "llama2" -- default nil
+			require("gen").setup({
+				-- model = "codellama",
+				model = "mistral",
 			})
 		end,
 	},
